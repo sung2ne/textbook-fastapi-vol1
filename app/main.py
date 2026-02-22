bf9855c0bd561379912bab7d224d5e0f25a84e73
정필성 <jeongps@jeongpilseongs-MacBook-Pro.local>
2026-02-22 19:23:44 +0900

    part03/chapter-02: Hello FastAPI! 첫 번째 앱 및 엔드포인트 추가

--- app/main.py
+++ app/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get("/hello")
+def say_hello():
+    return {"greeting": "안녕하세요!"}
+
+@app.get("/items")
+def read_items():
+    return {"items": ["사과", "바나나", "오렌지"]}
